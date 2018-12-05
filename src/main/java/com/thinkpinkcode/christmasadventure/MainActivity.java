8a55633c7b5f577a70a96b6d2b347f54f8806ac1
Melanie Myers <MMelanieJ@gmail.com>
2018-12-05 10:38:45 -0500

    pull in user name

--- src/main/java/com/thinkpinkcode/christmasadventure/MainActivity.java
+++ src/main/java/com/thinkpinkcode/christmasadventure/MainActivity.java
@@ -2,12 +2,29 @@ package com.thinkpinkcode.christmasadventure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField;
+    private Button startButton;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameField = findViewById(R.id.nameEditText);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = nameField.getText().toString();
+            }
+        });
     }
 }
